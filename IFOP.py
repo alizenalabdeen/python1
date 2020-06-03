e9fafdb50a96519a4b08ee6d28db8e86132c7bac
alizenalabdeen <ali.zen.alabdeen@hotmail.com>
2020-06-03 19:03:33 +0300

    ifelse commit

---
+++ IFOP.py
@@ -0,0 +1,7 @@
+username = input('Enter your name:   ')
+password = input('Enter your password:  ')
+if username == "ali" and password == "564":
+    print('good job')
+
+else:
+    print('bad job') 
