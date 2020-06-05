35a7b898043dc7f642dc0d12e59b75aa9494678f
alizenalabdeen <ali.zen.alabdeen@hotmail.com>
2020-06-05 22:46:12 +0300

    DICT commit

---
+++ dict.py
@@ -0,0 +1,4 @@
+number_of_students = {"Arabic":3,"Since":5,"Math":6}
+print(number_of_students)
+students = {"Arabic":["ali","mohammed","anas"],"English":["adam","jack"]}
+print(students)
--- fopLP.py
+++ fopLP.py
@@ -1,4 +1,5 @@
 numbers = [1,2,3,4,5,6,7,8,9] #matrix of numbers
 print (numbers)
+print (numbers[0])
 for num in numbers: #loop to print the matrix
     print (num)
