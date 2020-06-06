c418cc23e6bd65e54990ebe4297f16feeaffd0a3
alizenalabdeen <ali.zen.alabdeen@hotmail.com>
2020-06-06 18:06:58 +0300

    FUN commit

---
+++ functons.py
@@ -0,0 +1,10 @@
+def mainas (number1,number2):
+    print (int(number1)-int (number2))
+
+frst = input("frist number : ")
+second = input ("second number : ")
+print(".............................")
+
+mainas(frst,second)
+
+
